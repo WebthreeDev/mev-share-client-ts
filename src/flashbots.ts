1c0458883ff5256462d3f831372e9ac79430c064
WebThreeDev <122597830+WebthreeDev@users.noreply.github.com>
2023-11-22 18:35:45 -0600

    Update flashbots.ts
    ```signMessageWithAuthSigner``` 
    Extracted the logic for signing the message into a separate functionfor reusability and better code organization.

--- src/flashbots.ts
+++ src/flashbots.ts
@@ -1,10 +1,10 @@
 import { id as ethersId, Wallet } from "ethers"
 
 export type JsonRpcData = {
-    id: number,
-    result?: any,
-    error?: {code: number, message: string},
-    jsonrpc: string,
+  id: number,
+  result?: any,
+  error?: { code: number, message: string },
+  jsonrpc: string,
 }
 
 /**
@@ -15,20 +15,35 @@ export type JsonRpcData = {
  * @returns Parameters of payload to send to Bundle API
  */
 export const getRpcRequest = async (params: any, method: string, authSigner: Wallet) => {
-    const body = {
-        params,
-        method,
-        id: 69,
-        jsonrpc: "2.0"
-    }
-    const signature = `${authSigner.address}:${await authSigner.signMessage(ethersId(JSON.stringify(body)))}`
-    const headers = {
-        'Content-Type': 'application/json',
-        'X-Flashbots-Signature': signature,
-    }
-    return {
-        headers,
-        signature,
-        body,
-    }
+  const body = {
+    params,
+    method,
+    id: 69,
+    jsonrpc: "2.0"
+  }
+
+  const signature = await signMessageWithAuthSigner(body, authSigner)
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Flashbots-Signature': signature,
+  }
+
+  return {
+    headers,
+    signature,
+    body,
+  }
+}
+
+/**
+ * Sign a message with the authSigner wallet.
+ * @param message - Message to sign
+ * @param authSigner - Wallet used to sign the message
+ * @returns Signature of the message
+ */
+export const signMessageWithAuthSigner = async (message: any, authSigner: Wallet) => {
+  const messageHash = ethersId(JSON.stringify(message))
+  const signature = await authSigner.signMessage(messageHash)
+  return `${authSigner.address}:${signature}`
 }
